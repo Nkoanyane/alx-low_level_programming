d663a2eae6bf72c96b869fc301e9a671cd7344e2
Nkoanyane <nkoanyane@gmail.com>
2023-03-16 23:00:58 +0200

    fixed 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -8,5 +9,5 @@
 int main(void)
 {
 	write(1, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	return (1); 
 }
