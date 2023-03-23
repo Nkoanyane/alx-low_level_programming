68e9c8181ca7f0354716fb0d17da5897fdc6b060
Nkoanyane <nkoanyane@gmail.com>
2023-03-24 00:50:58 +0200

    added is supper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper -checks for uppercase character
+ * @c: the character to be checked
+ * return : 1 if true otherwise 0
+ */
+
+int _isupper(int c)
+{
+	return (c>= 'A' && c<= 'Z');
+
+}
